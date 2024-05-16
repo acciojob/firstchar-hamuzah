a05192170c2c5709130926b96c9a71ebf158bbcb
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-16 12:25:47 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
   // your code here
+
+	if(text[0] === '' || text === ''){
+		return '';
+	}
+
+	return text[0];
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
